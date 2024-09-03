83ea7cd64fccb6792f894bda230e51d6fcdba9ef
YzaatKadyrkulov <yzaatkadyrkulov7@gmail.com>
2024-09-03 14:35:09 +0600

    first commit

---
+++ src/main/java/kg/mir/Mirproject/MirProjectApplication.java
@@ -0,0 +1,13 @@
+package kg.mir.Mirproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MirProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/mir/Mirproject/MirProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.mir.Mirproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
