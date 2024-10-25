e1b5be66f88ed7e3e3b459eae810cf92aa77ca58
YzaatKadyrkulov <yzaatkadyrkulov7@gmail.com>
2024-10-25 11:33:00 +0600

    added_scan_base_packages

--- src/main/java/kg/mir/Mirproject/MirProjectApplication.java
+++ src/main/java/kg/mir/Mirproject/MirProjectApplication.java
@@ -3,9 +3,8 @@ package kg.mir.Mirproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "kg.mir.Mirproject")
 public class MirProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MirProjectApplication.class, args);
     }
