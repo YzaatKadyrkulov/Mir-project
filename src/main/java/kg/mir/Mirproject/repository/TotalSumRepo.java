6178591e3e1cfb2c9573e6627b7846dd278b5ef4
daniel12142007 <danijelahatzanov@gmail.com>
2024-11-17 16:40:28 +0600

    Add percent sum for admin

--- src/main/java/kg/mir/Mirproject/repository/TotalSumRepo.java
+++ src/main/java/kg/mir/Mirproject/repository/TotalSumRepo.java
@@ -1,5 +1,6 @@
 package kg.mir.Mirproject.repository;
 
+import kg.mir.Mirproject.dto.AdminResponse;
 import kg.mir.Mirproject.entities.TotalSum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +12,13 @@ public interface TotalSumRepo extends JpaRepository<TotalSum,Long> {
     Optional<TotalSum> getTotalSumById(Long id);
     @Query("SELECT t FROM TotalSum t")
     List<TotalSum> getAllTotalSum();
+    @Query("""
+            select new kg.mir.Mirproject.dto.AdminResponse(
+            t.totalSum,
+            cast(t.totalSum * 0.03 as double),
+            cast(t.totalSum * 0.02 as double),
+            cast(t.totalSum * 0.01 as double)
+            )from TotalSum t
+            """)
+    AdminResponse getTotalSumAndPercent(Long id);
 }
